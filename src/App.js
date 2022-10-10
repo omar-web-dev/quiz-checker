eab1637dcd84761b06efecc220b80cd8059ffe8e
omarwebdeve <omarf977865@gmail.com>
2022-10-10 20:47:01 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center h-[100vh] w-full">
+      <h1 className='text-3xl '>Change Drak Mode</h1>
+    </div>
+  );
+}
+
+export default App;
